5cc2824c222f9de286ee383e06520978537a3eb7
Yurii Myronov <yuramiron16@gmail.com>
2025-08-18 22:16:28 +0200

    add sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/image", "@nuxt/ui", "@nuxt/content"],
+  modules: ["@nuxt/image", "@nuxt/ui", "@nuxt/content", "@nuxtjs/sitemap"],
   css: ["~/assets/css/main.css"],
 
   app: {
