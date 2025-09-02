9513bf4f824796cd58b6f71afcad1e19da24471d
Yurii Myronov <yuramiron16@gmail.com>
2025-09-02 20:07:16 +0200

    add scripts for django

--- public/scripts/django/start.sh
+++ public/scripts/django/start.sh
@@ -2,13 +2,16 @@
 set -e
 
 # Check if directory is empty
-if [ "$(ls -A)" ]; then
+if [ "$(ls -A . 2>/dev/null)" != "" ]; then
     read -p "There are files in the current directory. Continue and perhaps overwrite them? (Y/N): " USER_CONFIRM < /dev/tty
     if [ "$USER_CONFIRM" != "Y" ] && [ "$USER_CONFIRM" != "y" ]; then
         echo "Canceled!"
         exit 1
     fi
 fi
+
+# Get project name
+read -p "Project name: " USER_INPUT < /dev/tty
 
 # Delete needed files
 [ -d ".venv" ] && rm -rf .venv
@@ -21,34 +24,26 @@ python3 -m venv .venv
 # Activate virtual environment
 source .venv/bin/activate
 pip install --upgrade pip
-pip install fastapi uvicorn
+pip install django
 
 # Get packages versions
-FASTAPI_VERSION=$(pip show fastapi | grep Version | awk '{print $2}')
-UVICORN_VERSION=$(pip show uvicorn | grep Version | awk '{print $2}')
+DJANGO_VERSION=$(pip show django | grep Version | awk '{print $2}')
 
 # Generate requirements.txt
-echo "fastapi==$FASTAPI_VERSION" > requirements.txt
-echo "uvicorn==$UVICORN_VERSION" >> requirements.txt
+echo "django==$DJANGO_VERSION" > requirements.txt
 
 # Create project structure
 mkdir -p src
 
-# Generate main.py
-cat > src/main.py << EOF
-from fastapi import FastAPI
+# Generate project
+django-admin startproject $USER_INPUT src
 
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI!"}
-EOF
-
-echo "FastAPI Start App generated successfully"
-
+echo
 echo "To activate virtual environment, run:"
 echo "    source .venv/bin/activate"
 
-echo "To start FastAPI application, run:"
-echo "    uvicorn src.main:app --host 0.0.0.0 --port 8000"
+echo "Open source directory, run:"
+echo "    cd src"
+
+echo "To start Django application, run:"
+echo "    python manage.py runserver"
