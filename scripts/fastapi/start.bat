5df5c13d8f7ea7d6b4b8b73f288f829faa65da86
Meizuno <Meizuno@users.noreply.github.com>
2025-09-11 16:15:31 +0000

    deploy: 14e4c0414ca096143e0aa221100f53ac22b244ed

--- scripts/fastapi/start.bat
+++ scripts/fastapi/start.bat
@@ -22,14 +22,17 @@ REM Create python virtual environment
 python -m venv .venv
 
 REM Install dependencies
-.\.venv\Scripts\python.exe -m pip install fastapi uvicorn
+.\.venv\Scripts\python.exe -m pip install fastapi uvicorn structlog
 
 REM Generate requirements.txt
 for /f "tokens=2 delims=: " %%A in ('.\.venv\Scripts\python.exe -m pip show fastapi ^| findstr Version') do set FASTAPI_VERSION=%%A
 for /f "tokens=2 delims=: " %%A in ('.\.venv\Scripts\python.exe -m pip show uvicorn ^| findstr Version') do set UVICORN_VERSION=%%A
+for /f "tokens=2 delims=: " %%A in ('.\.venv\Scripts\python.exe -m pip show structlog ^| findstr Version') do set STRUCTLOG_VERSION=%%A
+
 
 echo fastapi==%FASTAPI_VERSION%>requirements.txt
 echo uvicorn==%UVICORN_VERSION%>>requirements.txt
+echo uvicorn==%STRUCTLOG_VERSION%>>requirements.txt
 
 REM Generate project
 mkdir src
@@ -43,6 +46,28 @@ echo.>> main.py
 echo @app.get("/") >> main.py
 echo def read_root(): >> main.py
 echo     return {"message": "Hello, FastAPI!"} >> main.py
+echo.>> main.py
+
+REM Generate logger.py
+echo import logging >> logger.py
+echo import structlog >> logger.py
+echo.>> logger.py
+echo structlog.configure( >> logger.py
+echo     processors=[ >> logger.py
+echo         structlog.contextvars.merge_contextvars, >> logger.py
+echo         structlog.processors.add_log_level, >> logger.py
+echo         structlog.processors.StackInfoRenderer(), >> logger.py
+echo         structlog.dev.set_exc_info, >> logger.py
+echo         structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S", utc=False), >> logger.py
+echo         structlog.dev.ConsoleRenderer() >> logger.py
+echo     ], >> logger.py
+echo     wrapper_class=structlog.make_filtering_bound_logger(logging.NOTSET), >> logger.py
+echo     context_class=dict, >> logger.py
+echo     logger_factory=structlog.PrintLoggerFactory(), >> logger.py
+echo     cache_logger_on_first_use=False >> logger.py
+echo ) >> logger.py
+echo log = structlog.get_logger() >> logger.py
+echo.>> logger.py
 
 cd ..
 
